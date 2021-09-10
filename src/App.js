6ae17371bf9848eda312d1cd090c4f9b4e4b03a1
habib <habibdev96@gmail.com>
2021-09-10 16:39:09 -0600

    implemented react router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Globals from "./abstracts/Globals";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
 
 const App = () => {
   return (
     <>
       <Globals />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Booking" component={Booking} />
+        </Switch>
+      </Router>
     </>
   );
 };
